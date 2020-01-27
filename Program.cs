03e24b4148315a098ea54410f91bc0b1d5735f70
vyoma <vyoma98@gmail.com>
2020-01-27 15:09:35 +0530

    adding fourth program

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Learncs5
+{
+    public interface printText
+    {
+        void print();
+    }
+    class formatted : printText
+    {
+        public void print()
+        {
+            Console.WriteLine("Hiii!");
+        }
+    }
+
+    public class constructorinjection
+    {
+        private printText _printText;
+        public constructorinjection(printText p1)
+        {
+            this._printText = p1;
+        }
+        public void print()
+        {
+            _printText.print();
+        }
+    }
+
+    class content
+    {
+        static void Main(string[] args)
+        {
+            constructorinjection cs = new constructorinjection(new formatted());
+            cs.print();
+            Console.ReadKey();
+        }
+    }
+}
