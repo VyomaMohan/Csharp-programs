38b3d5a05a50592c6e00cd701f825b48702c41b6
vyoma <vyoma98@gmail.com>
2020-01-27 15:05:55 +0530

    adding third cs program

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Learncs4
+{
+
+    class Program
+    {
+        internal class Cat
+        {
+
+        }
+
+        internal class Dog
+        {
+
+        }
+        static async Task Main(string[] args)
+        {
+            var catMeowTask = catMeowAsync(1);
+            var dogBarkTask =  dogBarkAsync(1);
+            var alltask = new List<Task> { catMeowTask, dogBarkTask };
+            Console.WriteLine("tasks");
+            //while (alltask.any())
+            //{
+            //    task finished = await task.whenany(alltask);
+            //    if (finished == catmeowtask)
+            //    {
+            //        console.writeline("cat has meowed");
+            //    }
+            //    if (finished == dogbarktask)
+            //    {
+            //        console.writeline("dog has barked");
+            //    }
+            //    alltask.remove(finished);
+            //}
+
+        }
+
+        private static async Task<Cat> catMeowAsync(int num)
+        {
+            Console.WriteLine("Cat starts to meow");
+            await Task.Delay(300);
+            for (int i=0; i < num; i++)
+                Console.WriteLine("Meow");
+            return new Cat();
+        }
+
+        private static async Task<Dog> dogBarkAsync(int num)
+        {
+            Console.WriteLine("Dog starts to bark");
+            await Task.Delay(10);
+            for (int i = 0; i < num; i++)
+                Console.WriteLine("Bark");
+            return new Dog();
+        }
+
+    }
+}
