f3cf2328b2beb60b7908a453c533a1f748dfb063
vyoma <vyoma98@gmail.com>
2020-01-27 15:01:39 +0530

    adding second program

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Timers;
+
+namespace Learncs3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer time = new Timer(2000);
+            time.Elapsed += Time_Elapsed;
+
+            time.Start();
+            Console.ReadLine();
+        }
+
+        private static void Time_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.WriteLine(e.SignalTime);
+        }
+    }
+}
